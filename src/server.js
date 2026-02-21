25c06bfc728797b1556da199a600e3f1c86178fc
Buinov Vadym <karabincom@gmail.com>
2026-02-21 15:07:45 +0200

    feat: add notes routes

--- src/server.js
+++ src/server.js
@@ -1,3 +1,20 @@
-const message = 'cucumberser';
+import express from 'express';
+import 'dotenv/config';
 
-console.log(message);
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
