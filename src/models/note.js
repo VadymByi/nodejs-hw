07a480b74dce0890ae7a1c9f0575f93ac7b10d59
Buinov Vadym <karabincom@gmail.com>
2026-03-04 13:32:37 +0200

    feat: add validation with celebrate and implement notes filtering/pagination

--- src/models/note.js
+++ src/models/note.js
@@ -1,6 +1,6 @@
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
-import { TAGS } from '../constants/tags';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -26,6 +26,6 @@ const noteSchema = new Schema(
   },
 );
 
-noteSchema.index({ title: 'text' });
+noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
