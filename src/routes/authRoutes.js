6f023088daf5817c098aff41c3209bb5185492c5
Buinov Vadym <karabincom@gmail.com>
2026-03-10 16:19:13 +0200

    feat: implement user login route and controller

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser } from '../controllers/authController';
-import { registerUserSchema } from '../validations/authValidation';
+import { loginUser, registerUser } from '../controllers/authController';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validations/authValidation';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 
 export default router;
