a8a97863648a7fd3cbc1a789a53d6679d3588c50
Buinov Vadym <karabincom@gmail.com>
2026-03-13 16:29:41 +0200

    feat: integrate Cloudinary for media storage

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,18 @@
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
+import { saveFileToCoudinary } from '../utils/saveFileToCloudinary.js';
 
 export const updateUserAvatar = async (req, res) => {
   if (!req.file) {
     throw createHttpError(40, 'No file');
   }
 
-  res.status(200).json({ url: '' });
+  const result = await saveFileToCoudinary(req.file.buffer, req.user._id);
+
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: result.secure_url },
+    { new: true },
+  );
+  res.status(200).json({ url: updatedUser.avatar });
 };
