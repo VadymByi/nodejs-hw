4e83b00107ee40b91bcab7603e6b40978a077cd8
Buinov Vadym <karabincom@gmail.com>
2026-03-13 15:43:15 +0200

    feat: add user avatar upload functionality

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -24,6 +25,7 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use(authRoutes);
+app.use(userRoutes);
 app.use(notesRoutes);
 app.use(notFoundHandler);
 app.use(errors());
