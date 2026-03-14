6790b3556f0979d494f3ed79568193babeed20ec
Buinov Vadym <karabincom@gmail.com>
2026-03-14 12:46:36 +0200

    fix: update avatar upload logic and fix 500 error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,38 +1,27 @@
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
-import { saveFileToCoudinary } from '../utils/saveFileToCloudinary.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
-export const updateUserAvatar = async (req, res) => {
-  if (!req.file) {
-    throw createHttpError(400, 'No file');
+export const updateUserAvatar = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      throw createHttpError(400, 'No file');
+    }
+
+    const result = await saveFileToCloudinary(req.file.buffer, req.user._id);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar: result.secure_url },
+      { new: true },
+    );
+
+    if (!updatedUser) {
+      throw createHttpError(404, 'User not found');
+    }
+
+    res.status(200).json({ url: updatedUser.avatar });
+  } catch (error) {
+    next(error);
   }
-
-  const result = await saveFileToCoudinary(req.file.buffer, req.user._id);
-
-  const updatedUser = await User.findByIdAndUpdate(
-    req.user._id,
-    { avatar: result.secure_url },
-    { new: true },
-  );
-  res.status(200).json({ url: updatedUser.avatar });
 };
-
-// export const updateUserAvatar = async (req, res) => {
-//   if (!req.file) {
-//     throw createHttpError(400, 'No file');
-//   }
-
-//   try {
-//     const result = await saveFileToCoudinary(req.file.buffer, req.user._id);
-
-//     const updatedUser = await User.findByIdAndUpdate(
-//       req.user._id,
-//       { avatar: result.secure_url },
-//       { new: true },
-//     );
-
-//     res.status(200).json({ url: updatedUser.avatar });
-//   } catch (error) {
-//         throw createHttpError(500, 'Failed to upload avatar to cloud');
-//   }
-// };
